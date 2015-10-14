07590a9aaea16bb82f0d97737f5d857c38b1ad42
Andy Shih <daan.shih@gmail.com>
2015-10-14 12:01:05 +0800

    Initial Commit

---
+++ ASCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ASCamera
+//
+//  Created by 施安宏 on 2015/10/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ASCamera/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ASCamera
+//
+//  Created by 施安宏 on 2015/10/14.
+//
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
